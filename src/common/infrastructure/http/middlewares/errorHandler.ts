277370a1e5a809a47ba5191b0c51377ec8a19e3b
OtavioGelain <otavioluizgelain@gmail.com>
2026-02-22 22:56:50 -0300

    adding middlewares error

--- src/common/infrastructure/http/middlewares/errorHandler.ts
+++ src/common/infrastructure/http/middlewares/errorHandler.ts
@@ -1,10 +1,14 @@
+import { AppError } from "@/common/domain/errors/app-error";
 import { Request, Response, NextFunction } from "express";
 
 //middlewares especificos para tratamento de erro costumam receber 4 parametros
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction): Response{
     //Verifica se o erro/err pertence a classe nativa do Javascript 'Error' 
-    if(err instanceof Error){
-        return res.status(400).json({error: err.message})
+    if(err instanceof AppError){
+        return res.status(err.statusCode).json({error: err.message})
     }
+
+    console.log(err)
+
     return res.status(500).json({status: 'error', message: "Internal server error"})
 }
